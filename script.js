9fb7d3ef79862e357a8a0e8fab79ba76442a0be5
Beimnet Zewdu <zbeimnet@gmail.com>
2019-11-10 16:45:23 +0300

    1st version UI completed

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#checkButton").click(function() {
+        $('#checkModal').modal('show');
+    });
+    $("#hashButton").click(function() {
+        $('#hashModal').modal('show');
+    });
+})
